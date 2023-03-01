d205dfef64dfe07f26e58b2d924f8435e9511368
Ash <acst52@gmail.com>
2023-03-01 14:13:37 -0500

    added comment to connection.js since no longer running on Heroku

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
+  ? new Sequelize(process.env.JAWSDB_URL) // if running on Heroku, if not:
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
